96d36ae60d7fa697bb3dc08040a149c17f890a57
deckey <divanovic3d@gmail.com>
2021-03-05 22:13:37 +0100

    chupdated main func

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import settings
-import headlines
-if __name__ == '__main__':
-    headlines.app.run(host="0.0.0.0", port=settings.PORT, debug=settings.DEBUG)
+from headlines import app
+
+def headlines(request):
+    app.run(host="0.0.0.0", port=settings.PORT, debug=settings.DEBUG)
